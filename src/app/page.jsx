9f20ed6419144f665d768b13ae5a5d8feb653488
ABDUL SHARIQ KALIM <shariqkalim123@gmail.com>
2024-06-27 07:49:35 +0530

    first-commit

---
+++ src/app/page.jsx
@@ -0,0 +1,31 @@
+// App.jsx or any parent component
+
+"use client";
+
+import { useEffect } from "react";
+import AntiChess from "./AntiChess";
+import { toast } from "react-toastify";
+
+function page() {
+  useEffect(() => {
+    toast.info("Player 1 - start!", {
+      position: "top-center",
+      closeButton: false,
+    });
+    toast.info("Click on any peice to start the game!", {
+      position: "top-center",
+      closeButton: false,
+    });
+  });
+  return (
+    <div className="App">
+      <h1>Anti-Chess Game</h1>
+      <AntiChess />
+      <footer>
+        <p className="copyright">Copyright @AbdulShariqKalim(STCET)</p>
+      </footer>
+    </div>
+  );
+}
+
+export default page;
